f2ec7def901e555d44561c305cd23f198730dc37
ZONG-JIA GU <kabuto412rock@gmail.com>
2025-09-20 14:22:28 +0800

    Implement Todo model with JSON serialization

--- lib/features/todos/todo_model.dart
+++ lib/features/todos/todo_model.dart
@@ -1,1 +1,15 @@
+class Todo {
+  final String id;
+  final String title;
+  final bool done;
 
+  Todo({required this.id, required this.title, required this.done});
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      done: json['done'] as bool,
+    );
+  }
+}
